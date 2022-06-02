ec94126a342e356e51d48a958bb7b4096b94a483
kant003 <gonzalezm.angel@gmail.com>
2022-06-02 12:52:25 +0200

    location y lat lon

--- index.js
+++ index.js
@@ -23,13 +23,13 @@ app.get('/RapidAPIHost', function (req, res) {
 });
 
 
-app.get('/weather', async(req, res) => {
+app.get('/weather', async (req, res) => {
     var ip = req.headers['x-forwarded-for'] ||
-     req.socket.remoteAddress ||
-     null;
-     console.log(req.headers['x-forwarded-for'])
-     console.log(req.socket.remoteAddress )
-     console.log('ip:',ip)
+        req.socket.remoteAddress ||
+        null;
+    console.log(req.headers['x-forwarded-for'])
+    console.log(req.socket.remoteAddress)
+    console.log('ip:', ip)
 
     /* console.log(req.headers['x-forwarded-for'],
      req.socket.remoteAddress)
@@ -60,13 +60,93 @@ app.get('/weather', async(req, res) => {
         condition_text: data.current.condition.text,
         condition_spanishDayText: weatherConditions.find(c => c.code === data.current.condition.code).languages[0].day_text,
         condition_spanishNightText: weatherConditions.find(c => c.code === data.current.condition.code).languages[0].night_text,
-        icon:weatherConditions.find(c => c.code === data.current.condition.code).icon,
+        icon: weatherConditions.find(c => c.code === data.current.condition.code).icon,
 
     }
     res.json(toSend)
 })
 
-app.get('/weather/:location', async(req, res) => {
+
+app.get('/forecast', async (req, res) => {
+    const { lat, lon } = req.query;
+    console.log(lat, lon)
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Host': process.env.RapidAPIHost,
+            'X-RapidAPI-Key': process.env.RapidAPIKey
+        }
+    };
+    const url = `https://weatherapi-com.p.rapidapi.com/forecast.json?days=5&q=${lat},${lon}`
+    const response = await fetch(url, options)
+    const data = await response.json()
+    const toSend = data;
+    try {
+
+        toSend.current.condition.condition_spanishDayText = weatherConditions.find(c => c.code === data.current.condition.code).languages[0].day_text
+        toSend.current.condition.condition_spanishNightText = weatherConditions.find(c => c.code === data.current.condition.code).languages[0].night_text
+    } catch (e) {
+        console.log(e)
+    }
+
+    res.json(toSend)
+
+})
+
+app.get('/forecast/:location', async (req, res) => {
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Host': process.env.RapidAPIHost,
+            'X-RapidAPI-Key': process.env.RapidAPIKey
+        }
+    };
+    const url = 'https://weatherapi-com.p.rapidapi.com/forecast.json?days=5&q=' + req.params.location
+    const response = await fetch(url, options)
+    const data = await response.json()
+    const toSend = data;
+    try {
+        toSend.current.condition.condition_spanishDayText = weatherConditions.find(c => c.code === data.current.condition.code).languages[0].day_text
+        toSend.current.condition.condition_spanishNightText = weatherConditions.find(c => c.code === data.current.condition.code).languages[0].night_text
+    } catch (e) {
+        console.log(e)
+    }   
+        /*const toSend = {
+            name: data.location.name,
+            country: data.location.country,
+            region: data.location.region,
+            lat: data.location.lat,
+            lon: data.location.lon,
+            temp_c: data.current.temp_c,
+            is_day: data.current.is_day,
+            wind_kph: data.current.wind_kph,
+            precip_mm: data.current.precip_mm,
+            humidity: data.current.humidity,
+            cloud: data.current.cloud,
+            condition_text: data.current.condition.text,
+            condition_spanishDayText: weatherConditions.find(c => c.code === data.current.condition.code).languages[0].day_text,
+            condition_spanishNightText: weatherConditions.find(c => c.code === data.current.condition.code).languages[0].night_text,
+            icon:weatherConditions.find(c => c.code === data.current.condition.code).icon,
+            forecast: data.forecast.forecastday.map(f => ({
+                date: f.date,
+                day: new Date(f.date_epoch * 1000).getDate(),
+                date_epoch: f.date_epoch,
+                avgtemp_c: f.day.avgtemp_c,
+                icon: f.day.condition.icon,
+                hours: f.hour.map(h => ({
+                    hour: new Date(h.time_epoch * 1000).getHours(),
+                    date: h.time,
+                    temp_c: h.temp_c,
+                    icon: h.condition.icon,
+                }))
+            })),
+    
+            
+        }*/
+        res.json(toSend)
+
+})
+app.get('/weather/:location', async (req, res) => {
     const options = {
         method: 'GET',
         headers: {
@@ -93,7 +173,7 @@ app.get('/weather/:location', async(req, res) => {
         condition_text: data.current.condition.text,
         condition_spanishDayText: weatherConditions.find(c => c.code === data.current.condition.code).languages[0].day_text,
         condition_spanishNightText: weatherConditions.find(c => c.code === data.current.condition.code).languages[0].night_text,
-        icon:weatherConditions.find(c => c.code === data.current.condition.code).icon,
+        icon: weatherConditions.find(c => c.code === data.current.condition.code).icon,
 
     }
     res.json(toSend)
@@ -116,7 +196,7 @@ const weatherConditions = await preLoadWeatherConditions()
 //const weathers = await preLoadWeathers()
 //console.log(weatherConditions)
 const run = async () => {
-    await app.listen(process.env.PORT || 3000)
+    await app.listen(process.env.PORT || 4000)
     console.log('Server starter')
 }
 run().catch(error => console.log('Error to start:' + error))
